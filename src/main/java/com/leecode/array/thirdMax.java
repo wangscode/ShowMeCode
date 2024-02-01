fe81acf73be47a4c5c4d66e6d29f3346c3661ce1
wangs <332775212@qq.com>
2024-02-01 12:19:30 +0800

    leeCode刷题

---
+++ src/main/java/com/leecode/array/thirdMax.java
@@ -0,0 +1,16 @@
+package com.leecode.array;
+
+/**
+ * 414. 第三大的数
+ * 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数
+ * 进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？
+ */
+public class thirdMax {
+
+    public static void main(String[] args) {
+
+    }
+    public int thirdMax(int[] nums) {
+        return 0;
+    }
+}
